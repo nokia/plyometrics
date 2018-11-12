ca826d7eed3b6678db137a8c3b0ef72f9777c2d3
Piotr Podusowski <podusowski@gmail.com>
2018-11-12 13:04:50 +0100

    time measurement

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include "nbench.hpp"
+#include <memory>
+
+void escape(void* p)
+{
+    asm volatile("" : : "g"(p) : "memory");
+}
 
 
 int main()
@@ -8,6 +14,17 @@ nbench::benchmark([](nbench::loop& loop)
 {
     while (loop)
     {
+        auto p = std::make_shared<int>(5);
+        escape(p.get());
+    }
+});
+
+nbench::benchmark([](nbench::loop& loop)
+{
+    while (loop)
+    {
+        auto p = std::make_unique<int>(5);
+        escape(p.get());
     }
 });
 
--- nbench.hpp
+++ nbench.hpp
@@ -1,5 +1,7 @@
 #include <vector>
 #include <functional>
+#include <chrono>
+#include <iostream>
 
 namespace nbench
 {
@@ -8,8 +10,24 @@ struct loop
 {
     operator bool()
     {
+        _end = clock::now();
+        _iterations++;
+        if (_end - _start > std::chrono::seconds{2})
+            return false;
         return true;
     }
+
+    auto iteration_time() const
+    {
+        return (_end - _start) / _iterations;
+    }
+
+private:
+    using clock = std::chrono::high_resolution_clock;
+
+    clock::time_point _start = clock::now();
+    clock::time_point _end;
+    std::size_t _iterations = 0;
 };
 
 template<class F>
@@ -17,6 +35,7 @@ void benchmark(F&& f)
 {
     loop l;
     f(l);
+    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(l.iteration_time()).count() << "ns" << std::endl;
 }
 
 }
