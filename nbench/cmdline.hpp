928bfca08ec9d939743ae25d9498c41ec33c4cfe
Piotr Podusowski <podusowski@gmail.com>
2018-11-28 14:15:38 +0100

    >>= operator

--- nbench/cmdline.hpp
+++ nbench/cmdline.hpp
@@ -51,6 +51,12 @@ auto operator+(const T& a, const maybe<T>& b) -> maybe<T>
     return b ? a + *b : a;
 }
 
+template<class T, class F>
+auto operator>>=(const maybe<T>& m, F f) -> decltype(f(std::declval<T>()))
+{
+    return m ? f(*m) : none;
+}
+
 struct parse_result
 {
     maybe<std::string> result;
@@ -82,46 +88,36 @@ auto none_if_empty(const T& value) -> maybe<T>
 auto read_until_whitespace(const std::string& data) -> parse_result
 {
     if (data.empty())
-    {
         return parse_result{none, data};
-    }
 
     if (data[0] == ' ')
-    {
         return parse_result{none, data.substr(1)};
-    }
 
     const auto leaf = read_until_whitespace(data.substr(1));
-
     return parse_result{data.substr(0, 1) + leaf.result, leaf.data_left};
 }
 
 auto parse_option(const std::string& data) -> maybe<options>
 {
-    return none_if_empty(data).map([](auto data)
+    return none_if_empty(data) >>= [](auto data)
     {
         if (data[0] == '-')
         {
             options opts;
-            auto name = read_until_whitespace(data.substr(1));
-            auto value = read_until_whitespace(name.data_left);
+
+            const auto name = read_until_whitespace(data.substr(1));
+            const auto value = read_until_whitespace(name.data_left);
 
             if (name.result and value.result)
-            {
                 opts.options.emplace(*name.result, *value.result);
-            }
             else if (name.result)
-            {
                 opts.switches.emplace(*name.result);
-            }
             else
                 throw 1;
 
-            std::cout << "name: " << *name.result << "=" << *value.result << std::endl;
-
             return opts + parse_option(value.data_left);
         }
-    });
+    };
 }
 
 auto parse(int argc, const char* argv[])
