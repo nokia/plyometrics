0208073e3b9da5028735e0d294026c82fef2f800
Piotr Podusowski <podusowski@gmail.com>
2019-01-09 11:50:47 +0100

    basics of visualizer

---
+++ contrib/visualize.py
@@ -0,0 +1,38 @@
+#/usr/bin/env python3
+
+import json
+import sys
+from collections import defaultdict
+import matplotlib.pyplot as plt
+
+
+def visualize(grouped_data):
+    #print(grouped_data)
+    for name, data in grouped_data.items():
+        for type_name, results in data.items():
+            print(name, type_name, results)
+            x = [sample['number'] for sample in results]
+            y = [sample['time'] for sample in results]
+            plt.plot(x, y)
+            plt.show()
+
+
+def group(data):
+    benchmarks = defaultdict(lambda: defaultdict(list))
+    for b in data:
+        benchmarks[b["name"]][b["type"]].append(b)
+    return benchmarks
+
+
+def main():
+    try:
+        data = json.load(sys.stdin)
+        print(group(data))
+        visualize(group(data))
+    except json.decoder.JSONDecodeError as e:
+        print(e)
+        print("Did you forgot -x switch?")
+
+
+if __name__ == "__main__":
+    main()
