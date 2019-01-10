1bb5666c2fe3430d6a91e0688936fdbae54d1abf
Piotr Podusowski <podusowski@gmail.com>
2019-01-10 09:33:15 +0100

    clear cache function

--- nbench/compiler.hpp
+++ nbench/compiler.hpp
@@ -17,5 +17,15 @@ __attribute__((noinline)) void im_here()
     asm volatile("");
 }
 
+void clear_cache()
+{
+    static std::vector<char> memory(1024 * 1024 * 32);
+
+    for (auto& i : memory)
+    {
+        i = 0;
+        escape(&i);
+    }
+}
 
 }
