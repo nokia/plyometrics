8ae32e4fdb48b0bd44bb6f8508b59a3a21618cba
Podusowski Piotr <piotr.podusowski@nokia.com>
2019-01-21 13:20:19 +0100

    test various passing of sp

--- examples/example_basic.cpp
+++ examples/example_basic.cpp
@@ -40,6 +40,42 @@ BENCHMARK("allocate by malloc") = [](auto& loop)
     }
 };
 
+__attribute__((noinline)) void foo_value(std::shared_ptr<int> i)
+{
+    nbench::use(*i);
+}
+
+BENCHMARK("pass shared_ptr by value") = [](auto& loop)
+{
+    auto p = std::make_shared<int>(5);
+
+    while (loop)
+    {
+        foo_value(p);
+        foo_value(p);
+        foo_value(p);
+        foo_value(p);
+    }
+};
+
+__attribute__((noinline)) void foo_reference(const std::shared_ptr<int>& i)
+{
+    nbench::use(*i);
+}
+
+BENCHMARK("pass shared_ptr by reference") = [](auto& loop)
+{
+    auto p = std::make_shared<int>(5);
+
+    while (loop)
+    {
+        foo_reference(p);
+        foo_reference(p);
+        foo_reference(p);
+        foo_reference(p);
+    }
+};
+
 int main(int argc, const char* argv[])
 {
     nbench::run_all(argc, argv);
