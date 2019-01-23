59ed03ecf498aa3854494d1907ee2697c499bc70
Podusowski Piotr <piotr.podusowski@nokia.com>
2019-01-23 12:31:50 +0100

    some tests for the future

--- tests/options_tests.cpp
+++ tests/options_tests.cpp
@@ -1,6 +1,41 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-TEST_CASE("sample test")
+#include "nbench/cmdline.hpp"
+
+TEST_CASE("switch is recognized")
 {
+    const char* argv[] = {"app", "-a"};
+    const auto options = nbench::parse_options(std::size(argv), argv);
+    CAPTURE(options);
+
+    CHECK(options.has_switch("-a"));
 }
+
+TEST_CASE("long switch is recognized")
+{
+    const char* argv[] = {"app", "--long"};
+    const auto options = nbench::parse_options(std::size(argv), argv);
+    CAPTURE(options);
+
+    CHECK(options.has_switch("--long"));
+}
+
+TEST_CASE("named option is recognized")
+{
+    const char* argv[] = {"app", "-a", "a value"};
+    const auto options = nbench::parse_options(std::size(argv), argv);
+    CAPTURE(options);
+
+    CHECK(options.option("-a") == "a_value");
+}
+
+TEST_CASE("long named option is recognized")
+{
+    const char* argv[] = {"app", "--long", "a value"};
+    const auto options = nbench::parse_options(std::size(argv), argv);
+    CAPTURE(options);
+
+    CHECK(options.option("--long") == "a_value");
+}
+
