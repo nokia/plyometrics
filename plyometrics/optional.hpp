dae83affe9a1e981085c0e4839aab3134c8df14f
Piotr Podusowski <podusowski@gmail.com>
2019-12-05 22:26:34 +0100

    fix for optional check

--- plyometrics/optional.hpp
+++ plyometrics/optional.hpp
@@ -3,31 +3,25 @@
 // std::optional is somehow new and in older compilers it's hidden
 // under std::experimental
 
-#ifdef __has_include
-    #if __has_include(<optional>)
+#ifdef __cpp_lib_optional
 
-        #include <optional>
+    #include <optional>
 
-        namespace plyometrics
-        {
-            using std::optional;
-            using std::nullopt;
-        }
+    namespace plyometrics
+    {
+        using std::optional;
+        using std::nullopt;
+    }
 
-    #elif __has_include(<experimental/optional>)
-
-        #include <experimental/optional>
-
-        namespace plyometrics
-        {
-            using std::experimental::optional;
-            using std::experimental::nullopt;
-        }
-
-    #else
-        #error "can't locate any standard optional"
-    #endif
 #else
-    #error "can't test for optional class"
+
+    #include <experimental/optional>
+
+    namespace plyometrics
+    {
+        using std::experimental::optional;
+        using std::experimental::nullopt;
+    }
+
 #endif
 
